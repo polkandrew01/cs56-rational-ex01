ef116aed315362d40e91a15e965dfe89929738bc
Andrew Polk <polk@umail.ucsb.edu>
2017-10-04 23:30:46 -0700

    finished rational-ex05

--- src/Rational.java
+++ src/Rational.java
@@ -3,30 +3,6 @@ public class Rational {
     private int num;
     private int denom;
 
-    public Rational() {
-	this.num = 1;
-	this.denom = 1;
-    }
-
-    public Rational(int num, int denom) {
-	this.num = num;
-	this.denom = denom;
-    }
-
-    @Override
-    public String toString() {
-	return num + "/" + denom;
-    }
-
-    public static void main (String [] args) {
-	Rational r = new Rational(5,7);
-	System.out.println("r.getNumerator()=" + r.getNumerator());
-	System.out.println("r.getNumerator()=" + r.getDenominator());
-    }
-
-    public int getNumerator() { return this.num;}
-    public int getDenominator() {return this.denom;}
-	
 	/**
 		greatest common divisor of a and b
 		@param a first number
@@ -34,6 +10,41 @@ public class Rational {
 		@return gcd of a and b
 	*/
 	public static int gcd(int a, int b) {
-		return -42;
+		if(a==0)
+			return b;
+		else if (b==0)
+			return a;
+		else
+			return gcd(b%a, a);
 	}
+	
+    public Rational() {
+		this.num = 1;
+		this.denom = 1;
+    }
+
+    public Rational(int num, int denom) {
+		this.num = num;
+		this.denom = denom;
+		if(num != 0) {
+			int gcd = Rational.gcd(num, denom);
+			this.num /= gcd;
+			this.denom /= gcd;
+		}
+    }
+
+    public String toString() {
+		if(denom == 1 || num == 0)
+			return "" + num;
+		return num + "/" + denom;
+    }
+
+	public int getNumerator() { return this.num;}
+    public int getDenominator() {return this.denom;}
+	
+    public static void main (String [] args) {
+		Rational r = new Rational(5,7);
+		System.out.println("r.getNumerator()=" + r.getNumerator());
+		System.out.println("r.getNumerator()=" + r.getDenominator());
+    }
 }
